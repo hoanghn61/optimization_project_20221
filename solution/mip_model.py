f9c4dc88f334b7c9ab73cd289eaa126a89666574
Trung1812 <89788023+Trung1812@users.noreply.github.com>
2023-02-09 11:28:28 +0700

    Update mip_model.py

--- solution/mip_model.py
+++ solution/mip_model.py
@@ -1,6 +1,7 @@
-#MIP model
+#MIP model opt time
 from ortools.linear_solver import pywraplp
-import sys
+
+from itertools import combinations
 def input_data(file_name):
   with open(file_name) as f:
     n,m = [int(x) for x in f.readline().split()]
@@ -20,25 +21,26 @@ def input_data(file_name):
       cost.append([float(x) for x in f.readline().split()])
     return n, m, time, worker_for_task, start_time, presequisite, cost
 
-input_data('test.txt')
 def main():
   solver = pywraplp.Solver.CreateSolver('SCIP')
   if not solver:
     return
     
-  #decisions variables
+  #decision variables
   num_task, num_worker, timee, worker_for_task, start_time, presequisite, cost = input_data(input())
+  print(timee)
   x = {}
+  infinity = solver.infinity()
   for worker in range(num_worker):
     for task in range(num_task):
       x[worker, task] = solver.IntVar(0,1,'')
   st = {}
   for i in range(num_task):
-    st[i] = solver.NumVar(0.0, solver.infinity() ,'')
+    st[i] = solver.IntVar(0.0, solver.infinity() ,'')
   y = {}
   for i in range(num_task):
-    y[i] = solver.IntVar(0,num_worker, '')
-  salary = solver.NumVar(0.0, max([sum(row_cost) for row_cost in cost]),'')
+    y[i] = solver.IntVar(0,num_worker-1, '')
+  salary = solver.NumVar(0, infinity, '')
   M = 100000
 
   #constraints
@@ -49,32 +51,61 @@ def main():
         solver.Add(x[worker,task] == 0)
   #presequisite time
   for pair in presequisite:
-    solver.Add(st[pair[0]]+timee[pair[0]] <= st[pair[1]])
+    solver.Add(st[pair[0]]+ timee[pair[0]] <= st[pair[1]])
   #the task must be done by one worker
   for task in range(num_task):
     solver.Add(solver.Sum([x[worker,task] for worker in range(num_worker)]) == 1)
   #the start time requirement
   for task in range(num_task):
-    for worker in range(worker):
+    for worker in range(num_worker):
       solver.Add(st[task] >= start_time[worker]+M*(x[worker,task]-1))
+
   #the obvious
   for task in range(num_task):
     for worker in range(num_worker):
+      gamma = solver.BoolVar('')
       solver.Add(y[task]-worker <= M*(1-x[worker, task]))
       solver.Add(worker-y[task] <= M*(1-x[worker,task]))
+      solver.Add(y[task] >= worker + 1 - M*gamma - M*x[worker,task])
+      solver.Add(y[task] <= worker - 1 +M*(1-gamma) +M*x[worker,task])
   #if two tasks are performed by the same worker, they have to be non-overlapping
-  alpha = {}
-  delta = {}
+
   for i in range(num_task-1):
-    alpha[i] = []
-    delta[i] = []
     for j in range(i+1,num_task):
-      alpha[i].append(solver.IntVar(0,1,''))
-      delta[i].append(solver.IntVar(0,1,''))
-      solver.Add(y[i]-y[j] <= M*(1-alpha[i][j-i-1]))
-      solver.Add(y[j]-y[i] <= M*(1-alpha[i][j-i-1]))
-      solver.Add(st[i] >= st[j] + timee[j] - M*delta[i][j-i-1] - M*(1-alpha[i][j-i-1]))
-      solver.Add(st[i] <= st[j] - timee[i] + M*(1-delta[i][j-i-1]) + M*(1-alpha[i][j-i-1]))
+      x1 = solver.BoolVar('')
+      x2 = solver.BoolVar('')
+      solver.Add(st[i] + timee[i] <= st[j] + M*(1-x1))
+      solver.Add(st[j] + timee[j] <= st[i] + M*(1-x2))
+
+      b = solver.BoolVar('')
+      solver.Add(x1 + x2 <= b*M)
+      solver.Add(x1 + x2 +(1-b)*M >= 1)
+
+      t0 = solver.IntVar(0,1,'')
+      t1 = solver.IntVar(0,1,'')
+      t2 = solver.IntVar(0,1,'')
+
+      solver.Add(y[i] + t0 <= y[j] + M*t1)
+      solver.Add(y[j] + t0 <= y[i] + M*t2)
+      solver.Add(t1 + t2 == t0)
+
+      solver.Add(b == 1 - t0)
+
+
+  #salary = max salary among k workers
+
+  h = {}
+  U = max(start_time) + sum(timee)
+  L = max(timee) +min(start_time)
+  for task in range(num_task):
+    h[task] = solver.BoolVar('')
+    solver.Add(st[task] + timee[task] >= L)
+    solver.Add(st[task] + timee[task] <= U)
+    solver.Add(finish_time >= st[task] + timee[task])
+    solver.Add(finish_time <= st[task] + timee[task] + M*(1-h[task]))
+  solver.Add(sum([h[task] for task in range(num_task)]) == 1)
+
+
   #salary = max salary among k workers
   L = 0
   U = max([sum([cost[i][j] for i in range(num_task)]) for j in range(num_worker)])
